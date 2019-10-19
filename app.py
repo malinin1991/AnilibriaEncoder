573f92f0418532f5abf8db93ab393f350f4ebaa2
GeeKaZ0iD <maliinevgeniy@gmail.com>
2019-10-20 02:11:20 +0700

    различные фиксы

--- app.py
+++ app.py
@@ -20,6 +20,10 @@ def encode_video(from_dir, to_dir, prepare_hevc):
         media_info = MediaInfo.parse(from_dir + file)
         fr_den = media_info.tracks[1].framerate_den
         fr_num = media_info.tracks[1].framerate_num
+        if fr_den is None or fr_num is None:
+            rate = media_info.tracks[1].frame_rate
+        else:
+            rate = '{framerate_num}/{framerate_den}'.format(framerate_num=fr_num, framerate_den=fr_den)
         if not prepare_hevc:
             ffmpeg_param = '-hide_banner ' \
                            '-i "{input}" ' \
@@ -29,9 +33,8 @@ def encode_video(from_dir, to_dir, prepare_hevc):
                            '-x265-params "level=5.1:crf=23:ref=6" ' \
                            '-acodec copy ' \
                            '-map 0 ' \
-                           '-r {framerate_num}/{framerate_den} ' \
-                           '-f matroska "{output}"'.format(input=from_dir+file, output=to_dir+file,
-                                                           framerate_num=fr_num, framerate_den=fr_den)
+                           '-r {rate} ' \
+                           '-f matroska "{output}"'.format(input=from_dir+file, output=to_dir+file, rate=rate)
         else:
             ffmpeg_param = '-hide_banner ' \
                            '-i "{input}" ' \
@@ -39,12 +42,12 @@ def encode_video(from_dir, to_dir, prepare_hevc):
                            '-c:v libx265 ' \
                            '-vf format=yuv420p10le ' \
                            '-x265-params "level=5.1:crf=23:ref=6" ' \
-                           '-r {framerate_num}/{framerate_den} ' \
+                           '-r {rate} ' \
                            '-an ' \
                            '-dn ' \
                            '-sn ' \
                            '-f matroska "{output}"'.format(input=from_dir + file, output=to_dir+'source\\' + file,
-                                                           framerate_num=fr_num, framerate_den=fr_den)
+                                                           rate=rate)
         process = subprocess.run('"{ffmpeg}" '.format(ffmpeg=ffmpeg)+ffmpeg_param, shell=True)
         if process.returncode == 0:
             print('DONE')
@@ -91,7 +94,8 @@ def fix_files(from_dir, to_dir):
         cmd = '"{mkvmerge}"'.format(mkvmerge=mkvmerge)+' -o "{output}" ' \
                        '--track-name 0:"Original [{nickname}]" --language 0:jpn --default-track 0:yes --forced-track 0:yes '\
                        '--track-name 1:AniLibria.TV --language 1:rus --default-track 1:yes --forced-track 1:yes --sync 1:{rel1} '\
-                       '--track-name 2:Original --language 2:jpn --default-track 2:no --forced-track 2:no --sync 2:{rel2} '.format(
+                       '--track-name 2:Original --language 2:jpn --default-track 2:no --forced-track 2:no --sync 2:{rel2} ' \
+                       '--no-track-tags --no-global-tags '.format(
                                         output=to_dir+mkv.replace('].mkv', '_HEVC].mkv'),
                                         rel1=rel[0], rel2=rel[1],
                                         nickname=nickname) \
@@ -152,11 +156,13 @@ def merge_hevc(from_dir, to_dir):
             order = '--track-order 1:0,0:1,0:2 '
         src0 = '--no-video ' \
                '--track-name 1:"AniLibria.TV" --language 1:rus --default-track 1:yes --forced-track 1:yes --sync 1:{rel1} ' \
-               '--track-name 2:"Original" --language 2:jpn --default-track 2:no --forced-track 2:no --sync 2:{rel2} '.format(
+               '--track-name 2:"Original" --language 2:jpn --default-track 2:no --forced-track 2:no --sync 2:{rel2} ' \
+               '--no-track-tags --no-global-tags '.format(
                                           rel1=rel[0],
                                           rel2=rel[1])
 
         src1 = '--track-name 0:"Original [{nickname}]" --language 0:jpn --default-track 0:yes --forced-track 0:yes ' \
+               '--no-track-tags --no-global-tags ' \
                '"{from_dir}{mkv}" '.format(from_dir=from_dir+'source\\', mkv=mkv, nickname=nickname)
         cmd = '"{mkvmerge}"'.format(mkvmerge=mkvmerge) + ' -o "{output}'.format(output=to_dir + mkv.replace('].mkv', '_HEVC].mkv" ')
                                                                                          + src0
@@ -171,9 +177,15 @@ def merge_hevc(from_dir, to_dir):
     return None
 
 
+def convert_to_opus(source, destination):
+    pass
+
+
 if need_encode:
     encode_video(fromdir, tmp_dir, prepare)
+if create_opus:
+    pass
 if need_merge:
     merge_hevc(tmp_dir, todir)
-else:
+if need_fix:
     fix_files(tmp_dir, todir)
--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 nickname = 'GeeKaZ0iD'      # Никнейм, который будет подставлен, как автор дорожки
 
-fromdir = r'F:\2019\Machikado Mazoku - AniLibria.TV [WEBRip 1080p]\\'     # Папка, из которой будем брать файлы.
-todir = r'D:\2019\Machikado Mazoku - AniLibria.TV [WEBRip 1080p HEVC]\\'  # Папка, в которую будем записывать готовое.
+fromdir = r'C:\from\\'     # Папка, из которой будем брать файлы.
+todir = r'D:\\'  # Папка, в которую будем записывать готовое.
 tmp_dir = r'C:\temp\\'   # Папка, в которую будут писаться временные файлы.
 
 ffmpeg = r'C:\Program Files\ffmpeg\bin\ffmpeg.exe'  # Путь до ffmpeg.exe
@@ -9,6 +9,8 @@ mkvpropedit = r'C:\Program Files\MKVToolNix\mkvpropedit.exe'     # Путь к m
 mkvmerge = r'C:\Program Files\MKVToolNix\mkvmerge.exe'   # Путь к mkvmerge.exe
 # В данном случае важно экранировать слэши (\\)
 
-need_encode = False      # Требуется ли кодирование видео?
+need_encode = True      # Требуется ли кодирование видео?
+need_fix = True
+create_opus = False
 prepare = False         # Необходимо ли подготовить файлы без звука. Только видеодорожка.
 need_merge = False      # Требуется ли объединить скодированную заранее видеодорожку с готовым релизом от технарей?
